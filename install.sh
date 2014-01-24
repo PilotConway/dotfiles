1c4107eef461dcb84b298525cc3042654ddb758c
John L Conway IV <john@johnconwayiv.com>
2014-01-24 13:21:59 -0500

    install now removes the tmp directory on completion

--- install.sh
+++ install.sh
@@ -15,16 +15,16 @@ mkdir -p $TMPDIR
 ##
 echo -n "Extracting Tarball..."
 # get the number of lines of this script 
-SKIP=`awk '/^__ARCHIVE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
+SKIP=`awk '/^__END_SCRIPT__/ { print NR + 1; exit 0; }' $0`
 # extract the lines after this script which will be the embedded tarball.
 tail -n+$SKIP $THIS | tar -xzf - -C $TMPDIR
 echo "Done"
 
 # remove the temp directory
-#rm -rf $TMPDIR
+rm -rf $TMPDIR
 
 # Exit and make sure the tarball below isn't run. 
 exit 0
 
 # Mark the end of this script and the beginning of the archive. 
-__ARCHIVE_FOLLOWS__
+__END_SCRIPT__
