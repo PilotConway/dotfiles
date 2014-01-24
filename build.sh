44af836494e6e55325c49b7a3744403daedf9f34
John L Conway IV <john@johnconwayiv.com>
2014-01-24 13:23:38 -0500

    Renamed install.sh to tarball_exec.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 FOLDERS="dotfiles/"
+FILES="install.sh"
 TAR="conway-dotfiles.tar.gz"
 EXEC="install-dotfiles"
+SELF_EXTRACTING_SCRIPT="tarball_exec.sh"
 
 if [[ -f $EXEC ]]; then 
     rm $EXEC
 fi
 
-tar -czf $TAR $FOLDERS
+tar -czf $TAR $FOLDERS $FILES
 
-cat install.sh $TAR > $EXEC
+cat $SELF_EXTRACTING_SCRIPT $TAR > $EXEC
 chmod a+x $EXEC 
 
 rm $TAR
